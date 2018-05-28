4053ecbbfe74b607f02cbf0a3467775feeba1e0c
ElskeKeimpema <204738@edu.rocfriesepoort.nl>
2018-05-28 07:45:50 +0200

    Added coins

---
+++ Seriousgame_Diabetes/Assets/Scripts/Coin.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "New Coin", menuName = "Coin")]
+public class Coin : ScriptableObject {
+
+    public int worth;
+    public Sprite coinSprite;
+}
